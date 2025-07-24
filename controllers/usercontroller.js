9935ea2fe51e9d607b13e9db986f93b859ba7c49
Pragya aditya <pragyaaditya62@gmail.com>
2025-07-24 08:42:40 +0530

    first

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -47,6 +47,15 @@ exports.updateUser = async (req, res) => {
 
     res.status(500).json({ error: 'Server error' });
   }
-
+// GET /api/users/:id
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id); // Mongoose query
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid ID format' });
+  }
+};
   
 };
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/usercontroller');
 router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
+router.get('/:id', userController.getUserById);
 
 
 
